aab7b385a75370daa1ae7052027b596f0a8f0624
Michael.lindre <michael.lindre@oodesigns.co.uk>
2026-01-02 09:19:26 +0000

    Implement FileLoader and FileLoaderException classes; add tests for file loading functionality

---
+++ src/test/java/com/oodesigns/cas/util/file/FileLoaderTest.java
@@ -0,0 +1,21 @@
+package com.oodesigns.cas.util.file;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FileLoaderTest {
+
+    @Test
+    void unableToLoadFileThrows(){
+        assertThrows(FileLoaderException.class, ()->new FileLoader("NoFile.txt"));
+    }
+
+    @Test
+    void loadAndReadText(){
+        final FileLoader fileLoader = new FileLoader("testfile.txt");
+        assertThat(fileLoader.toString()).contains("some test data");
+    }
+
+}
